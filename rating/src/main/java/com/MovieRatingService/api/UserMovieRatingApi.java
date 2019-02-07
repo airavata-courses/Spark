28ffb725149f6f8fe5b8b1cd01bca1741e5ede0f
SHWETA KULKARNI <kulsv1393@gmail.com>
2019-02-06 18:40:06 -0900

    cross origin issue

--- rating/src/main/java/com/MovieRatingService/api/UserMovieRatingApi.java
+++ rating/src/main/java/com/MovieRatingService/api/UserMovieRatingApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/usermovierating")
 public interface UserMovieRatingApi {
