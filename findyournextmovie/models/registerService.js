cab694c127153e775b3efbd8b094517d4ad2e52a
Bbaggines <shradha.baranwal6@gmail.com>
2019-03-01 15:08:04 -0900

    find ip from jenkins pipeline

--- findyournextmovie/models/registerService.js
+++ findyournextmovie/models/registerService.js
@@ -1,9 +1,10 @@
 var request = require('request');
-const ip = require('ip');
 
 function register() {
+  const args = require('minimist')(process.argv);
+
   request.post(
-      'http://149.165.169.102:5000/services/register?name=login&uri='+'http://' + ip.address() + ':8080',
+      'http://149.165.169.102:5000/services/register?name=login&uri='+'http://' + args['LOGIN_IP'] + ':8080',
       function (error, response, body) {
           if (!error && response.statusCode == 200) {
               console.log("success");
