12c60d6789216f58531bd3c06555f0b8da9ed88f
alakhsingh <alakhsingh2009@gmail.com>
2019-03-03 11:58:15 -0500

    Jenkins test build

--- rating/src/main/java/com/MovieRatingService/RatingApplication.java
+++ rating/src/main/java/com/MovieRatingService/RatingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RatingApplication {
-
+	//Jenkins test build
 	public static void main(String[] args) {
 		SpringApplication.run(RatingApplication.class, args);
 	}
