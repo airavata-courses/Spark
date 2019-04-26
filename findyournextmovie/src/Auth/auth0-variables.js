93a12060ac98108265cadea10444fa014300bdd9
kulsv <kulsv1393@gmail.com>
2019-04-26 11:31:37 -0400

    auth0 server config

--- findyournextmovie/src/Auth/auth0-variables.js
+++ findyournextmovie/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'findyournextmovie.auth0.com',
   clientId: 'nYTUW6KtB3TKI0pUCNGR0mE0kh05kxC6',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://149.165.169.90:80/callback'
 }
