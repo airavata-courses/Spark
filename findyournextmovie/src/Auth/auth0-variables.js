4b595ddd84cfd4b9a875e4f114975debe5a38a39
Shweta Vivek Kulkarni <kulsv1393@gmail.com>
2019-04-27 20:27:33 -0400

    #97

--- findyournextmovie/src/Auth/auth0-variables.js
+++ findyournextmovie/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'findyournextmovie.auth0.com',
   clientId: 'nYTUW6KtB3TKI0pUCNGR0mE0kh05kxC6',
-  // callbackUrl: 'http://149.165.169.90:80/callback'
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://149.165.169.90:80/callback'
+  //callbackUrl: 'http://localhost:3000/callback'
 }
